3330fac6bf98bd4c559379e731c00091234b96be
Wes Souza <hey@wes.dev>
2022-04-01 21:55:05 -0400

    cli: Configure typescript-test, add sample test

---
+++ src/utils/errors.test.ts
@@ -0,0 +1,33 @@
+import { instanceOfNodeError, nodeErrorCode } from './errors';
+
+class NodeError extends Error {
+  code?: string;
+}
+
+describe(instanceOfNodeError.name, () => {
+  it('returns true for node errors', () => {
+    const error = new NodeError('Error test');
+    error.code = 'Test';
+
+    expect(instanceOfNodeError(error, NodeError)).toEqual(true);
+  });
+
+  it('returns false for errors without code', () => {
+    const error = new Error('Error test');
+    expect(instanceOfNodeError(error, Error)).toEqual(false);
+  });
+
+  it('returns false for non errors', () => {
+    const error = 'string';
+    expect(instanceOfNodeError(error, Error)).toEqual(false);
+  });
+});
+
+describe(nodeErrorCode.name, () => {
+  it('returns the error code', () => {
+    const error = new NodeError('Error test');
+    error.code = 'Test';
+
+    expect(nodeErrorCode(error)).toEqual(error.code);
+  });
+});

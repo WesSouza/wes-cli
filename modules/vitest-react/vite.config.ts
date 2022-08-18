fdc4504d7696cc1d25e7de61ab1bad22dfd5a407
Wes Souza <hey@wes.dev>
2022-08-18 10:10:52 -0400

    vitest-react: Add new Vitest + React module

---
+++ modules/vitest-react/vite.config.ts
@@ -0,0 +1,16 @@
+/// <reference types="vitest" />
+/// <reference types="vite/client" />
+
+import { default as react } from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  test: {
+    css: true,
+    environment: 'jsdom',
+    globals: true,
+    setupFiles: './vitest.setup.ts',
+  },
+});
